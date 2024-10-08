8308c35cca3eec2fbd0b21a628f649b117b93a5d
Renan Victor <renan22rsw@gmail.com>
2024-10-08 12:55:00 -0300

    suspense on AnimePage

--- src/app/search/animes/page.tsx
+++ src/app/search/animes/page.tsx
@@ -3,11 +3,8 @@
 import React, { Suspense } from "react";
 import Loading from "@/components/Loading/Loading";
 import AnimePageContent from "@/components/Contents/Animes/AnimesContentPage";
-import { useSearchParams } from "next/navigation";
 
 export default function AnimePage() {
-  const search = useSearchParams();
-
   return (
     <Suspense fallback={<Loading />}>
       <AnimePageContent />
